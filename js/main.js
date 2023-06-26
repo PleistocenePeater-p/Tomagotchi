da32f0d63930b0a34c94d6ca85ba923a1f2f09b1
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-06-26 16:55:36 -0700

    Adding HTML buttons, class properties, class methods

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 class Tomagotchi {
+    constructor(hunger, sleepiness, boredom, age){
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
+        this.age = age;
+    }
+    isHungry(){}
+    isSleepy(){}
+    isBored(){}
 }
 
