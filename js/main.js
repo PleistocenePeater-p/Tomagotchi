9a3649a22e623bb38d33a19354b44493e1647e51
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-06-26 22:38:18 -0700

    Establishing event handler for buttons

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
+//Constants
+
+//App's State (variables)
 class Tomagotchi {
-    constructor(hunger, sleepiness, boredom, age){
+    constructor(hunger, sleepiness, boredom, age, image, name){
         this.hunger = hunger;
         this.sleepiness = sleepiness;
         this.boredom = boredom;
         this.age = age;
+        this.image = image;
+        this.name = name;
     }
     isHungry(){}
+    //+1 demerit, requests user to buff stat
     isSleepy(){}
+    //+1 demerit, requests user to buff stat
     isBored(){}
+    //+1 demerit, requests user to buff stat
 }
 
+let response {
+
+}
+
+
+
+//Cached Elements
+//Event Listeners
+document.getElementById("buttons").addEventListener("click", eventHandler)
+
+//Functions
+init();
+function init() {
+    const koromon = new Tomagotchi {
+        this.hunger = 0;
+
+    }
+}
+
+function eventHandler(evt){
+
+}
